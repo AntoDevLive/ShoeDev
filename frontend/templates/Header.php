7e38276cf7a37a38c73d197aaa7a4f3e5a689bec
antoniocp99 <antoniocastizo99@gmail.com>
2025-11-16 15:31:32 +0100

    Botón de iniciar sesióne ne l header

--- frontend/templates/Header.php
+++ frontend/templates/Header.php
@@ -8,7 +8,8 @@
                     <div class="flex items-center gap-6">
                         <a href="#" class="text-sm font-medium hover:text-orange-600 transition">Tienda</a>
                         <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Categorías</a>
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Ofertas</a>
+                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Contacto</a>
+
                         <button id="carrito-btn" class="cursor-pointer flex items-center gap-2 bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-500 transition">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -19,10 +20,18 @@
                             </svg>
                             Carrito
                         </button>
+
+                        <a id="carrito-btn" class="cursor-pointer flex items-center gap-2 bg-sky-600 text-white px-4 py-2 rounded hover:bg-sky-500 transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                            </svg>
+                            Iniciar Sesión
+                        </a>
                     </div>
+
                 </div>
             </div>
         </nav>
     </header>
-
-                
