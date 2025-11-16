213b08a32ae8c7b64c1253187e5b1b6cabfd8088
antoniocp99 <antoniocastizo99@gmail.com>
2025-11-16 16:09:57 +0100

    plantilla de tienda.php

--- frontend/templates/Header.php
+++ frontend/templates/Header.php
@@ -6,22 +6,7 @@
                         Shoe<span class="text-orange-600">Dev</span>
                     </a>
                     <div class="flex items-center gap-6">
-                        <a href="#" class="text-sm font-medium hover:text-orange-600 transition">Tienda</a>
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Categorías</a>
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Contacto</a>
-
-                        <button id="carrito-btn" class="cursor-pointer flex items-center gap-2 bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-500 transition">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                <path d="M17 17h-11v-14h-2" />
-                                <path d="M6 5l14 1l-1 7h-13" />
-                            </svg>
-                            Carrito
-                        </button>
-
-                        <a id="carrito-btn" class="cursor-pointer flex items-center gap-2 bg-sky-600 text-white px-4 py-2 rounded hover:bg-sky-500 transition">
+                        <a id="login-btn" class="cursor-pointer flex items-center gap-2 bg-sky-600 text-white px-4 py-2 rounded hover:bg-sky-500 transition">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
@@ -29,6 +14,20 @@
                             </svg>
                             Iniciar Sesión
                         </a>
+                        <a href="tienda.php" class="text-sm font-medium hover:text-orange-600 transition">Tienda</a>
+                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Categorías</a>
+                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-orange-600 transition">Contacto</a>
+
+                        <button id="carrito-btn" class="cursor-pointer flex items-center gap-2 bg-orange-600 text-white p-2 rounded-full hover:bg-orange-500 transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                                <path d="M17 17h-11v-14h-2" />
+                                <path d="M6 5l14 1l-1 7h-13" />
+                            </svg>
+                        </button>
+
                     </div>
 
                 </div>
